15f19d6817977568fefd8ea1863e10e565b7000f
Etta-Diego <onyinyechiikediego.gmail.com>
2023-11-03 11:28:16 +0100

    commit prompt user for imput

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_C
+#define MAIN_C
+
+/* Header files */
+#include <unistd.h>
+#include <stdio.h>
+
+
+#define UNUSED(x) (void)(x)
+
+
+#endif
---
+++ prompt_user.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point of the program
+ * @argc: counts the number of arguement
+ * @argv: stores the strings
+ *
+ * Return: 0 on success
+ */
+
+int main(int argc, char **argv)
+{
+
+	UNUSED(argc);
+	UNUSED(argc);
+
+	write(STDOUT_FILENO, "MudettaShell$ ", sizeof(char) * 14);
+	return (0);
+}
