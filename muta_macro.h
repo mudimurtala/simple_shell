2b8be52a33bb7d4dafee85ab6239b426e4e56430
mudimurtala <ibnmuhyideen95@gmail.com>
2023-11-07 00:17:43 -0800

    The simple shell project

--- muta_macro.h
+++ muta_macro.h
@@ -8,22 +8,55 @@
 #define HAULT __attribute__((unused))
 
 
+/* size of the memory preserved for _getline */
+#define MEMO_CAP 1024
 
+/************* Built-in command under HELP **************/
 
+#define ASS_CD_AGE "cd=\n"\
+"cd:\tcd [dir]\n\n"\
+"	Change the shell working directory.\n\n"\
+"	If no argument is given to cd the command will be interpreted\n"\
+"	as cd $HOME.\n"\
+"	if the argumenthelp is - (cd -), the command will be interpreted\n"\
+"	as cd $OLDPWD.\n\n"
 
+#define ASS_EXIT_AGE "exit=\n"\
+"exit:\texit [STATUS]\n\n"\
+"	Exit of the simple-shell.\n\n"\
+"	Exits the shell with a status of N.  If N is omitted, the exit status\n"\
+"	is that of the last command executed.\n\n"
 
+#define ASS_ENV_AGE "env=\n"\
+"env:\tenv \n\n"\
+"	Print environment.\n\n"\
+"	The env command will be print a complete list of enviroment variables.\n\n"
 
+#define ASS_SETENV_AGE "setenv=\n"\
+"setenv:\tsetenv VARIABLE VALUE\n\n"\
+"	Change or add an environment variable.\n\n"\
+"	initialize a new environment variable, or modify an existing one\n"\
+"	When there are not correct numbers of arguments print error message.\n\n"
 
+#define ASS_UNSETENV_AGE "unsetenv=\n"\
+"unsetenv:\tunsetenv VARIABLE\n\n"\
+"	The unsetenv function deletes one variable from the environment.\n\n"\
+"	Wen there are not correct numbers of arguments print error message.\n\n"
 
-
-
-
-
-
-
-
-
-
+#define ASS_AGE "help=\n"\
+"help:\thelp [BUILTIN_NAME]\n\n"\
+"	Display information about builtin commands.\n\n"\
+"	Displays brief summaries of builtin commands.  If BUILTIN_NAME is\n"\
+"	specified, gives detailed help on all commands matching BUILTIN_NAME,\n"\
+"	otherwise the list of help topics is printed BUILTIN_NAME list.\n"\
+"	Arguments:\n\n"\
+"	BUILTIN_NAME specifiying a help topic.\n\n"\
+"	cd\t[dir]\n"\
+"	exit\t[status]\n"\
+"	env\n"\
+"	setenv\t[variable value]\n"\
+"	unset\t[variable]\n"\
+"	help\t[built_name]\n\n"
 
 
 
