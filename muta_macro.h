8733c98ca0836ddc21f1cd0f3cd2633c4715d30e
mudimurtala <ibnmuhyideen95@gmail.com>
2023-11-09 19:41:16 -0800

    simple shell project

--- muta_macro.h
+++ muta_macro.h
@@ -2,18 +2,18 @@
 #define MUTA_MACRO_H
 
 /* The Prompt the user will see */
-#define SHOW "mutaShell@ "
+#define USER_MESSAGE "mutashell@ "
 
 /* Represent an entity intentionally not used in the code */
-#define HAULT __attribute__((unused))
+#define IGNORED_PARAM __attribute__((unused))
 
 
 /* size of the memory preserved for _getline */
-#define MEMO_CAP 1024
+#define BLOCK_CAPACITY 1024
 
 /************* Built-in command under HELP **************/
 
-#define ASS_CD_AGE "cd=\n"\
+#define CD_INFO_MESSAGE "cd=\n"\
 "cd:\tcd [dir]\n\n"\
 "	Change the shell working directory.\n\n"\
 "	If no argument is given to cd the command will be interpreted\n"\
@@ -21,29 +21,29 @@
 "	if the argumenthelp is - (cd -), the command will be interpreted\n"\
 "	as cd $OLDPWD.\n\n"
 
-#define ASS_EXIT_AGE "exit=\n"\
+#define EXIT_INFO_MESSAGE "exit=\n"\
 "exit:\texit [STATUS]\n\n"\
 "	Exit of the simple-shell.\n\n"\
 "	Exits the shell with a status of N.  If N is omitted, the exit status\n"\
 "	is that of the last command executed.\n\n"
 
-#define ASS_ENV_AGE "env=\n"\
+#define ENV_INFO_MESSAGE "env=\n"\
 "env:\tenv \n\n"\
 "	Print environment.\n\n"\
 "	The env command will be print a complete list of enviroment variables.\n\n"
 
-#define ASS_SETENV_AGE "setenv=\n"\
+#define SETENV_INFO_MESSAGE "setenv=\n"\
 "setenv:\tsetenv VARIABLE VALUE\n\n"\
 "	Change or add an environment variable.\n\n"\
 "	initialize a new environment variable, or modify an existing one\n"\
 "	When there are not correct numbers of arguments print error message.\n\n"
 
-#define ASS_UNSETENV_AGE "unsetenv=\n"\
+#define UNSETENV_INFO_MESSAGE "unsetenv=\n"\
 "unsetenv:\tunsetenv VARIABLE\n\n"\
 "	The unsetenv function deletes one variable from the environment.\n\n"\
 "	Wen there are not correct numbers of arguments print error message.\n\n"
 
-#define ASS_AGE "help=\n"\
+#define INFO_MESSAGE "help=\n"\
 "help:\thelp [BUILTIN_NAME]\n\n"\
 "	Display information about builtin commands.\n\n"\
 "	Displays brief summaries of builtin commands.  If BUILTIN_NAME is\n"\
